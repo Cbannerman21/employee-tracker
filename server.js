7ecb87daa2cfdf5f6510ff06f70c8850a6b982e9
Cbannerman <cbannerman21@gmail.com>
2022-08-07 12:11:08 -0400

    added server.js file structure and code

---
+++ server.js
@@ -0,0 +1,74 @@
+const mysql = require('mysql12');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'cbannerman21',
+    password: '5195',
+    database: 'db'
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    init();
+})
+
+const init = async () => {
+    try {
+        let answer = await inquirer.prompt({
+            name: 'choice',
+            type: 'list',
+            message: 'Please make a selection from the list:',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Emplolyees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role',
+                'Exit'
+            ]
+        });
+        switch (answer.choice) {
+            case 'View Employees':
+                employeeView();
+                break
+
+            case 'View Departments':
+                departmentView();
+                break
+            
+            case 'View Roles':
+                roleview();
+                break
+
+            case 'Add Employees':
+                employeeAdd();
+                break
+
+            case 'Add Departments':
+                departmentAdd();
+                break
+
+            case 'Add Roles':
+                roleAdd();
+                break
+
+            case 'Update Employee Role':
+                employeeUpdate();
+                break
+
+            case 'Exit':
+                connection.end();
+                init();
+        };
+    } catch (err) {
+        console.log(err);
+        init();
+    };
+}
+
+init();
